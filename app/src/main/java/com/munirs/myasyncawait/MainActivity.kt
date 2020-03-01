3601c8a629b3fecb261c7ca1359d0c684f7aec09
Munir Hoque <munir.hoque@gmail.com>
2020-03-01 09:36:45 -0800

    modified starter project / initial commit

--- app/src/main/java/com/munirs/myasyncawait/MainActivity.kt
+++ app/src/main/java/com/munirs/myasyncawait/MainActivity.kt
@@ -11,5 +11,8 @@ import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
