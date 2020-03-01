1b237e80ea1d4f10d00155df5e3a3bd0ee6f7623
Munir Hoque <munir.hoque@gmail.com>
2020-03-01 09:33:18 -0800

    starter project/first commit

---
+++ app/src/main/java/com/munirs/myasyncawait/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.munirs.myasyncawait
+
+import android.app.ProgressDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window.PROGRESS_START
+import android.widget.ProgressBar
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+class MainActivity : AppCompatActivity() {
+
+}
